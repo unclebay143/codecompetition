72a7fd7c69934eabc083c4ab8fb095f9fa92d542
unclebay143 <ayodelesamueladebayo143@gmail.com>
2020-04-24 00:15:26 -0700

    REGISTRATION AND LOGIN FRONTEND
    (JAVACRIPT FUNCTION)

---
+++ codeapp/static/js/switch.js
@@ -0,0 +1,10 @@
+function regswitch(){
+    document.getElementById('a').style.display='block';
+    document.getElementById('b').style.display='none';
+
+}
+function loginswitch(){
+    document.getElementById('b').style.display='block';
+    document.getElementById('a').style.display='none';
+
+}
